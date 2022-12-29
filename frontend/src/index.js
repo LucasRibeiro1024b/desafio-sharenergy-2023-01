d7ff2fd8b73d412a00a0f60668c91f5652799273
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2022-12-29 19:15:59 -0300

    feat: add route /users.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { NotFound } from './components';
-import { Login } from './pages';
+import { Login, Users } from './pages';
 import { ContextProvider } from './contexts/Context';
 import reportWebVitals from './reportWebVitals';
 import './styles/index.css';
@@ -14,6 +14,11 @@ const myRouter = createBrowserRouter([
     element: <Login />,
     errorElement: <NotFound />,
   },
+  {
+    path: '/users',
+    element: <Users />,
+    errorElement: <NotFound />,
+  }
 ]);
 
 root.render(
