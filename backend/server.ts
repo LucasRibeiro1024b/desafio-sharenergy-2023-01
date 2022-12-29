0c95d4e52638b988ddc63744369c63ab13b6c150
devspacerj <rf2014souza@gmail.com>
2022-12-29 13:22:22 -0300

    29/12 começando projeto

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import  Express  from "express";
 import { Router } from "express";
 const app = Express();
@@ -11,20 +10,4 @@ app.listen(port,() =>{
 })
 
 
-
-=======
-import  Express  from "express";
-import { Router } from "express";
-const app = Express();
-const router = Router();
-const port = process.env.PORT_SERVER
-
-
-app.listen(port,() =>{
-  console.log(`Você está conectado na porta ${port}`)
-})
-
-
-
->>>>>>> 18839f247b47473befba7274cd343f681f952ed4
 export default {router};
