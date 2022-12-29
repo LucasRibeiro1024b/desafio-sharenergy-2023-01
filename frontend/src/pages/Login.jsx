07f6ceee63ca4773c669f1a3ca2ef83143d9894b
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2022-12-29 16:05:31 -0300

    feat: finish configuring remember me input.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,55 @@
 import React, { useContext, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Context } from '../contexts/Context';
+import { localStorage } from '../utils';
 import '../styles/Login.css';
+
+const KEY_LOCAL_STORAGE = 'remembered';
 
 function Login() {
   const { logged, setLogged } = useContext(Context);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const [validCredentials, setValidCredentials] = useState(true);  
+
+  useState(() => {
+    const { REACT_APP_USERNAME, REACT_APP_PASSWORD } = process.env;
+    const remembered = localStorage.getItem(KEY_LOCAL_STORAGE);
+    
+    if (remembered) {
+      setValidCredentials(true);
+      const { username: user, password: pass } = remembered;
+
+      if (user === REACT_APP_USERNAME && pass === REACT_APP_PASSWORD) {
+        setLogged(true);
+      } else {
+        setLogged(false);
+      }
+    }
+  }, []);
 
   function handleClick() {
-    console.log("Clicou");
+    const { REACT_APP_USERNAME, REACT_APP_PASSWORD } = process.env;
+
+    if (rememberMe) {
+      localStorage.setItem(KEY_LOCAL_STORAGE, { username, password });
+    }
+
+    if (username === REACT_APP_USERNAME && password === REACT_APP_PASSWORD) {
+      setValidCredentials(true);
+      setLogged(true);
+    } else {
+      setValidCredentials(false);
+      setLogged(false);
+    }
   }
 
   return (
     <main id='form-main'>
       { logged && <Navigate to="/users" replace={ true } /> }
       <h1>Login</h1>
+      { !validCredentials && <h2>Invalid credentials</h2>}
       <form>
         <section className="form-section">
           <label>Username&nbsp;&nbsp;</label>
