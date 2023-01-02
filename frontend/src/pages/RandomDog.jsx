a2bd8b247c67f9d97cf26f72592b5fc177c646aa
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-02 19:42:17 -0300

    feat: add random imgs of dogs and refresh button.

--- frontend/src/pages/RandomDog.jsx
+++ frontend/src/pages/RandomDog.jsx
@@ -2,21 +2,37 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Navbar } from '../components';
 import { Context } from '../contexts/Context';
-import { validateLoginLocalStorage } from '../utils';
-//  import '../styles/RandomDog.css';
+import { validateLoginLocalStorage, fetchApi } from '../utils';
+import '../styles/RandomDog.css';
 
 function RandomDog() {
   const { logged } = useContext(Context);
   const navigate = useNavigate();
+  const [imgURL, setImgURL] = useState('');
+  const BASE_URL = `https://random.dog/woof.json`;
 
   useEffect(() => {
     if (!(validateLoginLocalStorage() || logged)) navigate('/');
+
+    
+    fetchData();
   }, []);
+  
+  async function fetchData() {
+    const payload = await fetchApi.get(BASE_URL);
+
+    if (payload.url && !payload.url.endsWith('mp4')) setImgURL(payload.url);
+    else fetchData();
+  }
 
   return (
     <main id='random-dog-main'>
       <Navbar />
       <h1>Random dog</h1>
+      <button type='button' onClick={ fetchData }>Refresh</button>
+      <section id='random-dog-img-section'>
+        <img src={ imgURL } alt="Image of a random dog." />
+      </section>
     </main>
   );
 }
