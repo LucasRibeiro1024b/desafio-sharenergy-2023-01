e803f0112168adcc1546dadecac301b5a7ed5ca0
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-04 14:10:10 -0300

    feat: add clientsController.update.

--- backend/src/controllers/clients.js
+++ backend/src/controllers/clients.js
@@ -34,7 +34,30 @@ async function create(req, res) {
   return res.status(statusCode.noContent).end();
 }
 
+async function update(req, res) {
+  const { _id } = req.params;
+  const { name, email, phoneNumber, address, cpf } = req.body;
+
+  const payload = await clientsService
+    .update({ name, email, phoneNumber, address, cpf, _id });
+
+  if (typeof payload === 'string') {
+    return res
+      .status(statusCode.badRequest)
+      .json({ message: payload });
+  }
+
+  if (payload === null) {
+    return res
+      .status(statusCode.serverError)
+      .json({ message: 'Internal Server Error' });
+  }
+
+  return res.status(statusCode.noContent).end();
+}
+
 module.exports = {
   getAll,
   create,
+  update,
 };
