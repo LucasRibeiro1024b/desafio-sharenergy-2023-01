5d54e2f49e44bc775e271a9457e74329e4e97e95
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-03 22:05:29 -0300

    feat: add clientsService.create and a verification of the connection with the database.

--- backend/src/services/clients.js
+++ backend/src/services/clients.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
 const { Client } = require('../models');
+const { validateCreateBody } = require('../utils');
+
+function checkConnection() {
+  if (mongoose.connection.readyState !== 1) return null;
+
+  return true;
+}
 
 async function getAll() {
+  if (!checkConnection()) return null;
+
   try {
     const response = await Client.find();
 
@@ -8,9 +18,37 @@ async function getAll() {
 
   } catch (err) {
     console.error(err);
+
+    return null;
+  }
+}
+
+async function create({ name, email, phoneNumber, address, cpf }) {
+  if (!checkConnection()) return null;
+
+  if (!validateCreateBody({ name, email, phoneNumber, address, cpf })) {
+    return 'Bad Request';
+  }
+
+  try {
+    const response = await Client.create({
+      name: name.toLowerCase(),
+      email: email.toLowerCase(),
+      phoneNumber,
+      address,
+      cpf,
+    });
+
+    return response;
+
+  } catch (err) {
+    console.error(err);
+
+    return null;
   }
 }
 
 module.exports = {
   getAll,
+  create,
 };
