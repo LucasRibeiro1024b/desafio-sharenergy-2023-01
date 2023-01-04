7208ad8d580ca14ee0d12a899cd2c472ff537786
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-04 14:09:48 -0300

    feat: add clientsService.update.

--- backend/src/services/clients.js
+++ backend/src/services/clients.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Client } = require('../models');
-const { validateCreateBody } = require('../utils');
+const { validateClient } = require('../utils');
 
 function checkConnection() {
   if (mongoose.connection.readyState !== 1) return null;
@@ -26,7 +26,7 @@ async function getAll() {
 async function create({ name, email, phoneNumber, address, cpf }) {
   if (!checkConnection()) return null;
 
-  if (!validateCreateBody({ name, email, phoneNumber, address, cpf })) {
+  if (!validateClient({ name, email, phoneNumber, address, cpf })) {
     return 'Bad Request';
   }
 
@@ -48,7 +48,33 @@ async function create({ name, email, phoneNumber, address, cpf }) {
   }
 }
 
+async function update({ name, email, phoneNumber, address, cpf, _id }) {
+  if (!checkConnection()) return null;
+
+  if (!validateClient({ name, email, phoneNumber, address, cpf })) {
+    return 'Bad Request';
+  }
+
+  try {
+    const response = await Client.updateOne({ _id }, {
+      name: name.trim(),
+      email: email.toLowerCase().trim(),
+      phoneNumber: phoneNumber.trim(),
+      address: address.trim(),
+      cpf: cpf.trim(),
+    });
+
+    return response.modifiedCount === 1 ? true : 'Bad Request';
+
+  } catch (err) {
+    console.error(err);
+
+    return null;
+  }
+}
+
 module.exports = {
   getAll,
   create,
+  update,
 };
