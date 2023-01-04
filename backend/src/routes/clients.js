6a23f3049e3a9baccf0946ecb1acfcebf5fc0460
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-04 14:10:39 -0300

    feat: add route PUT /clients/_id.

--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -4,5 +4,6 @@ const { clientsController } = require('../controllers');
 
 router.get('/clients', clientsController.getAll);
 router.post('/clients', clientsController.create);
+router.put('/clients/:_id', clientsController.update);
 
 module.exports = router;
