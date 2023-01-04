d42f6e05b2ee0cb44f7cc636cad6a70498ed6d38
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-03 22:03:27 -0300

    feat: add route POST /clients.

--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { clientsController } = require('../controllers');
 
 router.get('/clients', clientsController.getAll);
+router.post('/clients', clientsController.create);
 
 module.exports = router;
