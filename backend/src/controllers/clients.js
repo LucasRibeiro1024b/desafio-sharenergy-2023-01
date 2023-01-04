a89db53d8ef286333f6dbbd2d930aabe4daec975
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-03 22:04:47 -0300

    feat: add clientsController.create and new HTTP status.

--- backend/src/controllers/clients.js
+++ backend/src/controllers/clients.js
@@ -4,9 +4,37 @@ const statusCode = require('./statusCode');
 async function getAll(_req, res) {
   const payload = await clientsService.getAll();
 
+  if (payload === null) {
+    return res
+      .status(statusCode.serverError)
+      .json({ message: 'Internal Server Error' });
+  }
+
   return res.status(statusCode.ok).json(payload);
+}
+
+async function create(req, res) {
+  const { name, email, phoneNumber, address, cpf } = req.body;
+
+  const payload = await clientsService
+    .create({ name, email, phoneNumber, address, cpf });
+
+  if (typeof payload === 'string') {
+    return res
+      .status(statusCode.badRequest)
+      .json({ message: payload });
+  }
+
+  if (payload === null) {
+    return res
+      .status(statusCode.serverError)
+      .json({ message: 'Internal Server Error' });
+  }
+
+  return res.status(statusCode.noContent).end();
 }
 
 module.exports = {
   getAll,
+  create,
 };
--- backend/src/controllers/statusCode.js
+++ backend/src/controllers/statusCode.js
@@ -1,3 +1,6 @@
 module.exports = {
   ok: 200,
+  noContent: 204,
+  badRequest: 400,
+  serverError: 500,
 };
