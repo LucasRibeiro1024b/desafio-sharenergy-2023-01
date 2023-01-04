f04fa73306a81c40f9409fb94cc28894612a8e1b
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-03 21:46:37 -0300

    feat: add validation of body to create a client.

---
+++ backend/src/utils/index.js
@@ -0,0 +1,5 @@
+const validateCreateBody = require('./validateCreateBody');
+
+module.exports = {
+  validateCreateBody,
+};
---
+++ backend/src/utils/validateCreateBody.js
@@ -0,0 +1,55 @@
+function validateName(name) {
+  if (name === undefined) return false;
+  if (name.trim() === '') return false;
+
+  return true;
+}
+
+function validateEmail(email) {
+  if (email === undefined) return false;
+  if (email.trim() === '') return false;
+  
+  email = email.toLowerCase();
+  const emailSplitted = email.split('@');
+  
+  if (emailSplitted.length !== 2) return false;
+  if (!emailSplitted[1].includes('.')) return false;
+  if (emailSplitted[0].includes('@')) return false;
+  if (emailSplitted[1].includes('@')) return false;
+
+  return true;
+}
+
+function validatePhoneNumber(phoneNumber) {
+  if (phoneNumber === undefined) return false;
+  if (phoneNumber.trim() === '') return false;
+  if (phoneNumber.length < 10 || phoneNumber.length > 11) return false;
+
+  return true;
+}
+
+function validateAddress(address) {
+  if (address === undefined) return false;
+  if (address.trim() === '') return false;
+
+  return true;
+}
+
+function validateCpf(cpf) {
+  if (cpf === undefined) return false;
+  if (cpf.trim() === '') return false;
+  if (cpf.length !== 11) return false;
+
+  return true;
+}
+
+function validateCreateBody({ name, email, phoneNumber, address, cpf }) {
+  if (!validateName(name)) return false;
+  if (!validateEmail(email)) return false;
+  if (!validatePhoneNumber(phoneNumber)) return false;
+  if (!validateAddress(address)) return false;
+  if (!validateCpf(cpf)) return false;
+  return true;
+}
+
+module.exports = validateCreateBody;
