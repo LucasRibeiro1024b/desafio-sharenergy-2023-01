0d6b0c880a2c4495aba2a7ad98f3a4662cf075d0
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-04 22:14:07 -0300

    feat: add cleaning of inputs, text to inform of invalid entries and toggle of modal.

--- frontend/src/components/ModalCreateClient.jsx
+++ frontend/src/components/ModalCreateClient.jsx
@@ -1,31 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchApi } from '../utils';
 
-function ModalCreateClient({ toggleModal }) {
+function ModalCreateClient({ toggleModal, fetchData }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [address, setAddress] = useState('');
+  const [invalidData, setInvalidData] = useState(false);
+  const POST_URL = 'http://localhost:3001/clients/';
+
+  async function handleClick() {
+    const data = { name, email, phoneNumber, cpf, address };
+    const result = await fetchApi.post(POST_URL, data);
+
+    if (result) {
+      setInvalidData(false);
+      fetchData();
+      toggleModal();
+      setName('');
+      setEmail('');
+      setPhoneNumber('');
+      setCpf('');
+      setAddress('');
+    } else {
+      setInvalidData(true);
+    }
+  }
+
+  function isNum(value) {
+    return (/^[0-9]*$/).test(value);
+  }
+
   return (
     <section id="clients-modal-create" style={{ display: 'none' }}>
       <div id="clients-modal-create-content">
+        { invalidData && <h3>Dados incompletos ou inválidos</h3> }
         <div>
           <p>Nome:</p>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="Gabriel Pinheiro Matiucci"
+            value={ name }
+            onChange={ ({ target }) => setName(target.value) }
+          />
         </div>
         <div>
           <p>E-mail:</p>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="gabriel_1505@outlook.com" 
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value) }
+          />
         </div>
         <div>
           <p>Telefone:</p>
-          <input type="text" maxLength=''/>
+          <input
+            type="text"
+            maxLength="11"
+            placeholder="13997060349"
+            value={ phoneNumber }
+            onChange={
+              ({ target: { value } }) => isNum(value) && setPhoneNumber(value)
+            }
+          />
         </div>
         <div>
           <p>CPF:</p>
-          <input type="text" />
+          <input
+            type="text"
+            maxLength="11"
+            placeholder="21365985564"
+            value={ cpf }
+            onChange={
+              ({ target: { value } }) => isNum(value) && setCpf(value)
+            }
+          />
         </div>
         <div>
           <p>Endereço:</p>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="Rua Frei Gaspar, 22, Centro, São Vicente/SP"
+            value={ address }
+            onChange={ ({ target }) => setAddress(target.value) }
+          />
         </div>
         <div id="clients-modal-create-content-buttons">
-          <button onClick={ () => toggleModal() }>Adicionar</button>
+          <button onClick={ handleClick }>Adicionar</button>
           <button onClick={ () => toggleModal() }>Cancelar</button>
         </div>
       </div>
