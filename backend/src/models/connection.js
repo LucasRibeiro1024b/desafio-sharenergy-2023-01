48e336e70105b167fb138d7e88d97a31fe7198d9
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-05 19:51:17 -0300

    feat: add a default connection string.

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const CONNECTION_STRING = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/desafiosharenergy-gpm';
 
 const connection = async function (string = CONNECTION_STRING) {
   try {
