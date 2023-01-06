ce6ea3aa4ee3b3eb594cd59cb0cf6bf33dac379f
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-06 14:42:58 -0300

    feat: add deletion of client.

--- frontend/src/components/Client.jsx
+++ frontend/src/components/Client.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { fetchApi } from '../utils';
+const { REACT_APP_CLIENTS_BASE_URL } = process.env;
 
-function Client({ client }) {
+function Client({ client, fetchData }) {
   const { name, email, phoneNumber, address, cpf, _id } = client;
+
+  async function handleDelete() {
+    if (window.confirm('Deseja realmente excluir este cliente?')) {
+      const result = await fetchApi.deleteOne(`${REACT_APP_CLIENTS_BASE_URL}/${_id}`);
+
+      if (result) fetchData();
+    }
+  }
 
   return (
     <div className="client-section">
@@ -14,7 +24,7 @@ function Client({ client }) {
       </div>
       <div className="client-buttons">
         <button type="button">Atualizar</button>
-        <button type="button">Excluir</button>
+        <button type="button" onClick={ handleDelete }>Excluir</button>
       </div>
     </div>
   );
