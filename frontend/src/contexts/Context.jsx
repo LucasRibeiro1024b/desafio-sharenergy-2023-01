adec19f323aa2b44827aa384f07d904a9dcf181f
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-01-06 17:27:59 -0300

    feat: add states to control the inputs of the ModalUpdateClient.

--- frontend/src/contexts/Context.jsx
+++ frontend/src/contexts/Context.jsx
@@ -4,7 +4,29 @@ const Context = createContext();
 
 function ContextProvider({ children }) {
   const [logged, setLogged] = useState(false);
-  const myContext = { logged, setLogged };
+  const [clientToUpdateName, setClientToUpdateName] = useState('');
+  const [clientToUpdateEmail, setClientToUpdateEmail] = useState('');
+  const [clientToUpdatePhoneNumber, setClientToUpdatePhoneNumber] = useState('');
+  const [clientToUpdateCpf, setClientToUpdateCpf] = useState('');
+  const [clientToUpdateAddress, setClientToUpdateAddress] = useState('');
+  const [clientToUpdateId, setClientToUpdateId] = useState('');
+
+  const myContext = {
+    logged,
+    setLogged,
+    clientToUpdateName,
+    setClientToUpdateName,
+    clientToUpdateEmail,
+    setClientToUpdateEmail,
+    clientToUpdatePhoneNumber,
+    setClientToUpdatePhoneNumber,
+    clientToUpdateCpf,
+    setClientToUpdateCpf,
+    clientToUpdateAddress,
+    setClientToUpdateAddress,
+    clientToUpdateId,
+    setClientToUpdateId,
+  };
 
   return (
     <Context.Provider value={ myContext }>
