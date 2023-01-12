a9b70fa9ee0348e5e6149da4f861ba8878bce9d8
Giovana Inez Vieira <gioivieira@gmail.com>
2023-01-11 21:16:11 -0300

    Start of creating StatusCodePage logic.

--- desafio-sharenergy-2023-01/src/pages/StatusCodePage/StatusCodePage.js
+++ desafio-sharenergy-2023-01/src/pages/StatusCodePage/StatusCodePage.js
@@ -1,15 +1,33 @@
 import Header from '../../components/Header/Header'
 import { StatusCodePageContainer } from './style'
+import useForm from '../../hooks/useForm'
 
-/* Em uma segunda página, o usuário deve ser capaz de selecionar um status code http qualquer, e, após a seleção, deve ser retornada uma imagem da api HTTP Cat 
-relacionada ao status escolhido, caso não exista tal imagem, deve ser retornada uma imagem de not found à critério de escolha do participante do desafio; */
+/* Em uma segunda página, o usuário deve ser capaz de selecionar um status code http qualquer, e, após a 
+seleção, deve ser retornada uma imagem da api HTTP Cat relacionada ao status escolhido, caso não exista tal 
+imagem, deve ser retornada uma imagem de not found à critério de escolha do participante do desafio; */
 
 const StatusCodePage=()=>{
+
+    const [form, onChangeInputs] = useForm({
+        statusCode: 100
+    })
+
+    const validStatusCodes = [100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 
+    208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 
+    406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 422, 423,
+    424, 425, 426, 428, 429, 431, 444, 449, 450, 499, 500, 501, 502, 503, 504, 505,
+    506, 507, 509, 510]
+
     return(
-        <StatusCodePageContainer>
+        <>
             <Header/>
-                <h1>Status Code</h1>
-        </StatusCodePageContainer>
+            <StatusCodePageContainer>
+                    <h1>Status Code</h1>
+                    <label htmlFor="statusCode">Choose Status Code</label>
+                    <input id="statusCode" name="statusCode" value={form.statusCode} onChange={onChangeInputs}/>
+                    <img src={`https://http.cat/${form.statusCode}`} alt="HTTP Cat API pic."></img>
+            </StatusCodePageContainer>
+        </>
     )
 }
 
--- desafio-sharenergy-2023-01/src/pages/StatusCodePage/style.js
+++ desafio-sharenergy-2023-01/src/pages/StatusCodePage/style.js
@@ -1,5 +1,16 @@
 import styled from "styled-components"
 
 export const StatusCodePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     text-align: center;
+
+    h4{
+        font-size: 1.5vw;
+    }   
+
+    h1{
+        margin-bottom: 1vw;
+    }
 `
