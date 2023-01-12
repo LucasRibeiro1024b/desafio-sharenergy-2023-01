5c021da59efdcb1ed602a21f13519c398abf8733
Giovana Inez Vieira <gioivieira@gmail.com>
2023-01-12 15:25:50 -0300

    Front-end file tweaks.

--- desafio-sharenergy-2023-01/src/components/Header/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export const HeaderContainer = styled.header`
-    background-color: var(--dark-orange);
-    text-align: center;
-    width: 100%;
-    height: 100px;
-    line-height: 100px;
-`
