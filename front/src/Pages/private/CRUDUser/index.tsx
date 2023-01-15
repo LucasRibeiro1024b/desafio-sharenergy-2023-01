5adaa50c96f78be564a24670d83fe05005f5fd8a
oliveiramiro2 <ramiromares95@gmail.com>
2023-01-14 22:44:54 -0300

    adjust on table CRUD

--- front/src/Pages/private/CRUDUser/index.tsx
+++ front/src/Pages/private/CRUDUser/index.tsx
@@ -1,87 +1,94 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+} from "@material-ui/core";
+import { Edit, Delete } from "@material-ui/icons";
 
 import Header from "../Components/Header";
 import Footer from "../../components/Footer";
-import "./index.css"
+import "./index.css";
 
 interface Column {
-    id: "name" | "code" | "population" | "size" | "density";
+    id: "name" | "email" | "phone" | "endereco" | "cpf" | "actions";
     label: string;
     minWidth?: number;
-    align?: "right";
+    align?: "center";
     /* eslint-disable-next-line */
     format?: (value: number) => string;
 }
 
 const columns: Column[] = [
-    { id: "name", label: "Name", minWidth: 170 },
-    { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+    { id: "name", label: "Nome", minWidth: 170 },
+    { id: "email", label: "E-mail", minWidth: 100 },
     {
-        id: "population",
-        label: "Population",
+        id: "phone",
+        label: "phone",
         minWidth: 170,
-        align: "right",
-        format: (value: number) => value.toLocaleString("en-US"),
+        align: "center",
     },
     {
-        id: "size",
-        label: "Size\u00a0(km\u00b2)",
+        id: "endereco",
+        label: "Endereço",
         minWidth: 170,
-        align: "right",
-        format: (value: number) => value.toLocaleString("en-US"),
+        align: "center",
     },
     {
-        id: "density",
-        label: "Density",
+        id: "cpf",
+        label: "CPF",
         minWidth: 170,
-        align: "right",
-        format: (value: number) => value.toFixed(2),
+        align: "center",
+    },
+    {
+        id: "actions",
+        label: "Ações",
+        minWidth: 170,
+        align: "center",
     },
 ];
 
 interface Data {
     name: string;
-    code: string;
-    population: number;
-    size: number;
-    density: number;
+    email: string;
+    phone: string;
+    endereco: string;
+    cpf: string;
+    actions?: any;
 }
 
 function createData(
     name: string,
-    code: string,
-    population: number,
-    size: number
+    email: string,
+    phone: string,
+    endereco: string,
+    cpf: string
 ): Data {
-    const density = population / size;
-    return { name, code, population, size, density };
+    return { name, email, phone, endereco, cpf };
 }
 
 const rows = [
-    createData("India", "IN", 1324171354, 3287263),
-    createData("China", "CN", 1403500365, 9596961),
-    createData("Italy", "IT", 60483973, 301340),
-    createData("United States", "US", 327167434, 9833520),
-    createData("Canada", "CA", 37602103, 9984670),
-    createData("Australia", "AU", 25475400, 7692024),
-    createData("Germany", "DE", 83019200, 357578),
-    createData("Ireland", "IE", 4857000, 70273),
-    createData("Mexico", "MX", 126577691, 1972550),
-    createData("Japan", "JP", 126317000, 377973),
-    createData("France", "FR", 67022000, 640679),
-    createData("United Kingdom", "GB", 67545757, 242495),
-    createData("Russia", "RU", 146793744, 17098246),
-    createData("Nigeria", "NG", 200962417, 923768),
-    createData("Brazil", "BR", 210147125, 8515767),
+    createData("user1", "user1", "1324171354", "alameda", "3287263"),
+    createData("user2", "user2", "1403500365", "alameda", "9596961"),
+    createData("user3", "user3", "60483973", "alameda", "301340"),
+    createData("user4", "user4", "327167434", "alameda", "9833520"),
+    createData("user5", "user5", "37602103", "alameda", "9984670"),
+    createData("user6", "user6", "25475400", "alameda", "7692024"),
+    createData("user7", "user7", "83019200", "alameda", "357578"),
+    createData("user8", "user8", "4857000", "alameda", "70273"),
+    createData("user9", "user9", "126577691", "alameda", "1972550"),
+    createData("user10", "user10", "126317000", "alameda", "377973"),
+    createData("user11", "user11", "67022000", "alameda", "640679"),
+    createData("user12", "user12", "67545757", "alameda", "242495"),
+    createData("user13", "user13", "146793744", "alameda", "17098246"),
+    createData("user14", "user14", "200962417", "alameda", "923768"),
+    createData("user15", "user15", "210147125", "alameda", "8515767"),
 ];
 
 const useStyles = makeStyles({
@@ -93,6 +100,13 @@ const useStyles = makeStyles({
     },
     container: {
         maxHeight: 440,
+    },
+    containActions: {
+        cursor: "pointer",
+    },
+    textActions: {
+        position: "relative",
+        bottom: 7,
     },
 });
 
@@ -148,7 +162,7 @@ const CRUDUser: React.FC = () => {
                                             hover
                                             role="checkbox"
                                             tabIndex={-1}
-                                            key={row.code}
+                                            key={row.cpf}
                                         >
                                             {columns.map(column => {
                                                 const value = row[column.id];
@@ -157,13 +171,43 @@ const CRUDUser: React.FC = () => {
                                                         key={column.id}
                                                         align={column.align}
                                                     >
-                                                        {column.format &&
-                                                        typeof value ===
-                                                            "number"
-                                                            ? column.format(
-                                                                  value
-                                                              )
-                                                            : value}
+                                                        {column.id ===
+                                                        "actions" ? (
+                                                            <div>
+                                                                <div
+                                                                    className={
+                                                                        classes.containActions
+                                                                    }
+                                                                >
+                                                                    <Edit color="action" />
+                                                                    <span
+                                                                        className={
+                                                                            classes.textActions
+                                                                        }
+                                                                    >
+                                                                        {" "}
+                                                                        Editar
+                                                                    </span>
+                                                                </div>
+                                                                <div
+                                                                    className={
+                                                                        classes.containActions
+                                                                    }
+                                                                >
+                                                                    <Delete color="error" />
+                                                                    <span
+                                                                        className={
+                                                                            classes.textActions
+                                                                        }
+                                                                    >
+                                                                        {" "}
+                                                                        Excluir
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                        ) : (
+                                                            value
+                                                        )}
                                                     </TableCell>
                                                 );
                                             })}
