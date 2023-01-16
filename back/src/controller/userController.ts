751f8dc418fe81db6a0761481453c9c1e69d3210
oliveiramiro2 <ramiromares95@gmail.com>
2023-01-16 05:00:05 -0300

    added logic to create a new user

--- back/src/controller/userController.ts
+++ back/src/controller/userController.ts
@@ -1,6 +1,15 @@
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
 
+export interface IDataUser {
+  name: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  addres: string;
+  password: string;
+}
+
 export class userController {
 
   async save(user: User) {
@@ -11,5 +20,17 @@ export class userController {
   async recoverAllUsers() {
     const users = await AppDataSource.mongoManager.find(User)
     return users
+  }
+
+  async recoverByEmail(email: string) {
+    const usernameCheck = await AppDataSource.mongoManager.findOne(User, {where: {email: email}})
+    return usernameCheck
+  }
+
+  async update(user: User, value: IDataUser) {
+    const update = await AppDataSource.mongoManager.update(User, user.id, {
+      ...value
+    })
+    return update
   }
 }
--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { userController } from "../controller/userController"
+import { userController, IDataUser} from "../controller/userController"
 import { User } from "../entity/User"
 
 export const userRouter = Router()
@@ -11,5 +11,47 @@ userRouter.get("/", async (req, res) => {
     res.json(response)
   }catch{
     res.status(404).json({message: "Something is wrong"})
-}
+  }
+})
+
+userRouter.post("/", async (req, res) => {
+  try{
+    const { name, password, email, cpf, addres, phone } = req.body
+
+    const checkIfEmailWasRegistred = await userCtrl.recoverByEmail(email)
+
+    if (checkIfEmailWasRegistred !== undefined){
+      const userForRegister = new User(name, email, phone, addres, cpf, password)
+
+      const userRegistred = await userCtrl.save(userForRegister)
+
+      if (userRegistred !== undefined) res.json(userRegistred)
+      res.status(401).json({message: "Something is wrong"})
+    }
+    res.status(401).json({message: "the email is even registred in system"})
+  }catch{
+    res.status(404).json({message: "Something is wrong"})
+  }
+})
+
+userRouter.post("/update", async (req, res) => {
+  try{
+    const { name, password, email, cpf, addres, phone } = req.body
+
+    const data: IDataUser = {
+      name, password, email, cpf, addres, phone
+    }
+
+    const userSelected = await userCtrl.recoverByEmail(email)
+
+    if (userSelected !== undefined) {
+      const userUpdated = await userCtrl.update(userSelected, data)
+
+      res.json(userUpdated)
+    }
+
+    res.status(401).json({message: "Sorry the user is not founded"})
+  }catch{
+    res.status(404).json({message: "Something is wrong"})
+  }
 })
